3889a2ef1e4448225ae374728821436cee82ccb5
zahwaihab <zahwa1703409@miuegypt.edu.eg>
2023-04-02 01:31:21 +0200

    first

---
+++ test.js
@@ -0,0 +1,12 @@
+const set1= new Array(3,1,7,9);
+const set2= new Array(2,4,1,9,3);
+let Distinct= new Array();
+let temp;
+}
+
+console.log(Distinct);
+let sum=0;
+for(var x=0 ; x<Distinct.length ; x++){
+    sum=sum+Distinct[x];
+}
+console.log(sum);
